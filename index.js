96a0669bcf6a3dbd33bb2350b83eaeb9d17aac97
Anam0927 <anam.devdes27@gmail.com>
2020-12-24 20:34:33 +0530

    Added tooltips

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const cls2 = Array.from(document.querySelectorAll('.cls-2'));
 const cls3 = Array.from(document.querySelectorAll('.cls-3'));
 const cls4 = Array.from(document.querySelectorAll('.cls-4'));
 const cls = [cls1, cls2, cls3, cls4];
-console.log(cls);
 
 const detailName = document.querySelector('.details p');
 const detailPrice = document.querySelector('.details h3');
@@ -57,28 +56,51 @@ function colorPicker(element, bool) {
   return color;
 }
 
+function showTooltip(p, name, price) {
+  let tooltip = document.createElement('div');
+  let tooltipP = document.createElement('p');
+  let tooltipH = document.createElement('h3');
+  tooltip.classList.add('tooltip');
+
+  tooltipP.innerHTML = name;
+  tooltipH.innerHTML = `${price} <span class="price-info">€/m<sup>2</sup></span>`;
+
+  tooltip.style.display = 'block';
+  tooltip.style.left = p.getBoundingClientRect().left + 'px';
+  tooltip.style.top = p.getBoundingClientRect().top + 'px';
+
+  document.body.appendChild(tooltip);
+  tooltip.appendChild(tooltipP);
+  tooltip.appendChild(tooltipH);
+}
+
+function hideTooltip() {
+  let tooltips = document.querySelectorAll('.tooltip');
+  tooltips.forEach((tooltip) => (tooltip.style.display = 'none'));
+}
+
 allPaths.forEach((path) => {
-  path.addEventListener('mouseover', () => {
-    hoverStateChanger(path, colorPicker(path, true));
+  path.addEventListener('mouseover', (e) => {
     let details = JSON.parse(path.dataset.details);
+    hoverStateChanger(path, colorPicker(path, true));
     detailName.innerText = details.name;
     detailPrice.innerHTML = `${details.price} <span class="price-info">€/m<sup>2</sup></span>`;
+    showTooltip(path, details.name, details.price);
   });
 
   path.addEventListener('mouseout', () => {
     hoverStateChanger(path, colorPicker(path, false));
+    hideTooltip();
   });
 });
 
 buttons.forEach((button, index) => {
   button.addEventListener('mouseover', () => {
-    console.log(cls[index]);
     cls[index].forEach((path) => {
       path.dispatchEvent(new MouseEvent('mouseover'));
     });
   });
   button.addEventListener('mouseout', () => {
-    console.log(cls[index]);
     cls[index].forEach((path) => {
       path.dispatchEvent(new MouseEvent('mouseout'));
     });
